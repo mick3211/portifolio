b64abe36048751284c257944e8b192ce55f4b408
Mickael <mickaelfelizardo2008@gmail.com>
2023-06-07 14:14:54 -0300

    initial setup

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+
+export const Header: React.FC = () => {
+    return (
+        <header>
+            <Link href="/">Mickael</Link>
+
+            <div></div>
+        </header>
+    );
+};
