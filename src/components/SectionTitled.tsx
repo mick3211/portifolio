007518a8e863d21b4015e963fa227914823d89a3
Mickael <mickaelfelizardo2008@gmail.com>
2023-07-20 14:46:19 -0300

    section title

--- src/components/SectionTitled.tsx
+++ src/components/SectionTitled.tsx
@@ -10,7 +10,10 @@ export const SectionTitled: React.FC<SectionTitledProps> = ({
 }) => {
     return (
         <section {...props}>
-            <div className="relative overflow-hidden section-titled-bg">
+            <div className="relative overflow-hidden">
+                <h2 className="w-fit text-xl font-semibold mb-6 relative after:absolute after:left-0 after:-bottom-1 after:w-10/12 after:h-[2px] after:bg-blue-500">
+                    {title}
+                </h2>
                 <div
                     className="absolute animate-fadeInUp -right-8 -z-50 h-0 w-0 rotate-90 select-none md:block hidden animate-fade-in-up"
                     aria-hidden
