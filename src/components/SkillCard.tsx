3021ab0f9157dbac7e34c708c79ad58eb8cc9c00
Mickael Rodrigues Felizardo <mickaelfelizardo2008@gmail.com>
2025-08-10 16:07:49 -0300

    fix: update SkillCard component for improved layout and add Flutter icon in Hero section

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -2,50 +2,50 @@ import Image from 'next/image';
 import React from 'react';
 
 interface SkillCardProps {
-    name: string;
-    image?: string;
-    level: string;
-    progress?: number;
-    color?: string;
-    tempo?: string;
+  name: string;
+  image?: string;
+  level: string;
+  progress?: number;
+  color?: string;
+  tempo?: string;
 }
 
 export const SkillCard: React.FC<SkillCardProps> = ({
-    image,
-    level,
-    name,
-    progress,
-    tempo,
+  image,
+  level,
+  name,
+  progress,
+  tempo,
 }) => {
-    return (
-        <div className="relative flex gap-4 bg-zinc-800/30 px-4 py-3 rounded-md shadow-md border-l-2 border-l-blue-500 md:backdrop-blur-md">
-            {image && (
-                <Image
-                    src={image}
-                    alt={name}
-                    width={64}
-                    height={64}
-                    className="max-h-16"
-                />
-            )}
-            <div className="flex-1">
-                <h3 className="font-bold text-xl sm:text-2xl">{name}</h3>
-                <p className="text-xs">
-                    {level} {tempo && <> &#x2022; </>} {tempo}
-                </p>
-                {progress && (
-                    <div className="h-1 bg-zinc-700 mt-2">
-                        <div
-                            className="h-[inherit] bg-blue-500 animate-extend"
-                            style={
-                                {
-                                    '--final-width': progress + '%',
-                                } as React.StyleHTMLAttributes<HTMLDivElement>
-                            }
-                        />
-                    </div>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className="relative flex gap-4 bg-zinc-800/30 px-4 py-3 rounded-md shadow-md border-l-2 border-l-blue-500 md:backdrop-blur-md">
+      {image && (
+        <Image
+          src={image}
+          alt={name}
+          width={64}
+          height={64}
+          className="max-h-16 aspect-square object-contain"
+        />
+      )}
+      <div className="flex-1">
+        <h3 className="font-bold text-xl sm:text-2xl">{name}</h3>
+        <p className="text-xs">
+          {level} {tempo && <> &#x2022; </>} {tempo}
+        </p>
+        {progress && (
+          <div className="h-1 bg-zinc-700 mt-2">
+            <div
+              className="h-[inherit] bg-blue-500 animate-extend"
+              style={
+                {
+                  '--final-width': progress + '%',
+                } as React.StyleHTMLAttributes<HTMLDivElement>
+              }
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
