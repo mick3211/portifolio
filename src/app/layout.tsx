a6fcf161d4b4e81e19a4127177654ff79a1e9787
Mickael Rodrigues Felizardo <mickaelfelizardo2008@gmail.com>
2025-08-10 15:11:11 -0300

    refactor: improve formatting and structure of layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,51 @@
 import { Header } from '@/components/Header';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { Viewport } from 'next';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-    title: {
-        default: 'Mickael Dev',
-        template: 'Mickael Dev | %s',
+  title: {
+    default: 'Mickael Dev',
+    template: 'Mickael Dev | %s',
+  },
+  description:
+    'Desenvolvedor front-end apaixonado por criar experiências da Web envolventes e fáceis de usar.',
+  themeColor: '#18181b',
+  keywords: [
+    'Desenvolvedor',
+    'Front-end',
+    'Freelancer',
+    'frontend',
+    'web',
+    'programador',
+  ],
+  authors: [
+    {
+      name: 'Mickael Felizardo',
+      url: 'https://linkedin.com/in/mickael-felizardo-5221431b4',
     },
-    description:
-        'Desenvolvedor front-end apaixonado por criar experiências da Web envolventes e fáceis de usar.',
-    themeColor: '#18181b',
-    keywords: [
-        'Desenvolvedor',
-        'Front-end',
-        'Freelancer',
-        'frontend',
-        'web',
-        'programador',
-    ],
-    authors: [
-        {
-            name: 'Mickael Felizardo',
-            url: 'https://linkedin.com/in/mickael-felizardo-5221431b4',
-        },
-    ],
-    colorScheme: 'dark',
+  ],
+};
+
+export const viewport: Viewport = {
+  colorScheme: 'dark',
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="pt-BR">
-            <body className={inter.className}>
-                <div className="container mx-auto px-4">
-                    <Header />
-                    {children}
-                </div>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>
+        <div className="container mx-auto px-4">
+          <Header />
+          {children}
+        </div>
+      </body>
+    </html>
+  );
 }
