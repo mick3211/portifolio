48d5e12fb281d44986465c1e54f6b0d6a160a68f
Mickael Rodrigues Felizardo <mickaelfelizardo2008@gmail.com>
2025-08-10 19:35:44 -0300

    add unoptimized flag to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            { hostname: 'images2.imgbox.com', pathname: '**' },
-            { hostname: 'play-lh.googleusercontent.com', pathname: '**' }
-        ],
-    },
+  images: {
+    remotePatterns: [
+      { hostname: 'images2.imgbox.com', pathname: '**' },
+      { hostname: 'play-lh.googleusercontent.com', pathname: '**' },
+    ],
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
